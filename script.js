47858b25004071d3f4319458a4203beec4aee3b2
giorgiobaeza <jorge.baeza92@hotmail.com>
2020-12-17 18:02:18 -0300

    primera actualización
    se agrega fecha de modificación, autor y objetivos

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+/* Fecha modificación: 17/12/2020
+Creado por J.Baeza G.
+esta es la página de nuestra fábrica de chocolates*/
 $(document).ready( function () {
     $('#tableid').DataTable();
 } );
